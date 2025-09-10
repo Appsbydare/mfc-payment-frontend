c26c48349c47549c74d87f781026a2ae894266aa
Appsbydare <appsbydarshana@gmail.com>
2025-09-10 18:36:22 +0530

    fix: remove duplicate Layout wrapper in DiscountManager page

--- src/pages/DiscountManager.tsx
+++ src/pages/DiscountManager.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import Layout from '../components/layout/Layout';
 import DiscountManager from '../components/DiscountManager';
 
 const DiscountManagerPage: React.FC = () => {
   return (
-    <Layout>
-      <div className="p-6">
-        <DiscountManager />
-      </div>
-    </Layout>
+    <div className="p-6">
+      <DiscountManager />
+    </div>
   );
 };
 
